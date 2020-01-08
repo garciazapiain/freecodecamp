4f19905cb619008aa2e9697757de473f93adfe71
garciazapiain <juangarciazapiain@gmail.com>
2020-01-08 19:01:35 +0100

    drum

---
+++ front-end/drum_machine/src/js/index.js
@@ -0,0 +1,18 @@
+console.log('It works');
+
+const handlePlusClick = () => {
+    onclick=document.getElementById('Q').style.backgroundColor = "red"
+    setTimeout(change,2000)
+    audio='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    audio.play()
+};
+
+const change = () => {
+    document.getElementById('Q').style.backgroundColor = "white"
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btnQ = document.querySelector('#Q');
+  btnQ.addEventListener('click', handlePlusClick);
+});
+
